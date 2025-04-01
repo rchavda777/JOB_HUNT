f5ce9de4152b436d35fc60f1f6bb490c323d3425
rahul_chavda <rahulchavda59904@gmail.com>
2025-04-01 13:08:50 +0530

    Added password reset functionality with email confirmation and password reset link. using django built-in views and templates.
    added templates for password reset

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from users import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', views.SignupPage, name = 'signup'),
@@ -8,4 +9,11 @@ urlpatterns = [
     path('recruiter_dashboard', views.recruiter_dashboard, name= "recruiter_dashboard"),
     path('logout/', views.Logout_user, name = 'logout'),
     path('jobseeker-profile/', views.update_user_profile, name='jobseeker_profile'),
+
+    # Password Reset URLs
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="reset_pass/password_reset.html"), name="password_reset"),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="reset_pass/password_reset_done.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset_pass/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="reset_pass/password_reset_complete.html"), name="password_reset_complete"),
+
 ]
